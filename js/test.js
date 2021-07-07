f8499c4832db006979443676f698c8e83061deaa
therealsiji <55184842+siji007@users.noreply.github.com>
2021-07-07 22:16:10 +0100

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,41 +1,81 @@
-var fullname = document.getElementById('fullName');
-var phoneNumber = document.getElementById('phoneNumber');
+var fullname = document.getElementById('fullName').value;
+var phoneNumber = document.getElementById('phoneNumber').value;
 var error = document.getElementById('error');
-var nextOfKin = document.getElementById('nextOfKin');
-var carPlateNumber = document.getElementById('plateNumber');
-var carColorn= document.getElementById('carColor');
+var nextOfKin = document.getElementById('nextOfKin').value;
+var carPlateNumber = document.getElementById('plateNumber').value;
+var carColor = document.getElementById('carColor').value;
+const fullNameError = document.getElementById("fullNameError")
 
-function validateTest(){
-    validateTest.preventDefault();
+function validate() {
+    fullNameError.innerHTML = ""
+    const regTestName = /^[a-z]+$/i;
+    const regNextOfKin = /^[A-Z][a-z] $/;
+    const regPlateNumber = /^A-ZA-ZA-Z[-]0-90-90-9A-Z $/;
 
-    var testName = /^[A-Z][a-b] $/;
-    var nextOfKin = /^[A-Z][a-b] $/;
-    var plateNumber = /^A-ZA-ZA-Z[-]0-90-90-9A-Z $/;
 
-
-    var errorIndex = [];
-    if(!fullname.match(testName)){
-        errorIndex.push('Name must Contain Letters Only');
-        error.innerHTML = errorIndex[0];
-
-    }else if(!nextOfKin.match(nextOfKin)){
-        errorIndex.push('Next Must Contain Only letters');
-        error.innerHTML = errorIndex[1];
-    }else if(phoneNumber > 11){
-        errorIndex.push('Phone Number Must Contain 11 digits Only');
-        error.innerHTML = errorIndex[2];
-    }else if(phoneNumber < 11){
-        errorIndex.push('Phone Number must be 11 digits only');
-        error.innerHTML = errorIndex[3];
-    }else if(carPlateNumber > 9 || carPlateNumber < 9){
-        errorIndex.push('Plate Number Length is incorrect');
-        error.innerHTML = errorIndex[4];
-    }else if(!carPlateNumber.match(plateNumber)){
-        errorIndex.push('Plate Number is incorrect');
-        error.innerHTML = errorIndex[5];
+    // var errorIndex = [];
+    if (!regTestName.test(fullname)) {
+        fullNameError.innerHTML = 'Name must Contain Letters Only'
+        // errorIndex.push('Name must Contain Letters Only');
+        // error.innerHTML = errorIndex[0];
+    } else {
+        fullNameError.innerHTML = ""
     }
 
-
-
+    // } else if (!regNextOfKin.test(nextOfKin)) {
+    //     errorIndex.push('Next Must Contain Only letters');
+    //     error.innerHTML = errorIndex[1];
+    // } else if (phoneNumber > 11) {
+    //     errorIndex.push('Phone Number Must Contain 11 digits Only');
+    //     error.innerHTML = errorIndex[2];
+    // } else if (phoneNumber < 11) {
+    //     errorIndex.push('Phone Number must be 11 digits only');
+    //     error.innerHTML = errorIndex[3];
+    // } else if (carPlateNumber.length > 9 || carPlateNumber.length < 9) {
+    //     errorIndex.push('Plate Number Length is incorrect');
+    //     error.innerHTML = errorIndex[4];
+    // } else if (!regPlateNumber.test(plateNumber)) {
+    //     errorIndex.push('Plate Number is incorrect');
+    //     error.innerHTML = errorIndex[5];
+    // }
 
 }
+// const myform = document.getElementById("myform")
+// myform.onsubmit(e => {
+//     e.preventDefault()
+//     console.log('submitted')
+// })
+// function validateTest(){
+//     validateTest.preventDefault();
+
+//     var testName = /^[A-Z][a-b] $/;
+//     var nextOfKin = /^[A-Z][a-b] $/;
+//     var plateNumber = /^A-ZA-ZA-Z[-]0-90-90-9A-Z $/;
+
+
+//     var errorIndex = [];
+//     if(!fullname.match(testName)){
+//         errorIndex.push('Name must Contain Letters Only');
+//         error.innerHTML = errorIndex[0];
+
+//     }else if(!nextOfKin.match(nextOfKin)){
+//         errorIndex.push('Next Must Contain Only letters');
+//         error.innerHTML = errorIndex[1];
+//     }else if(phoneNumber > 11){
+//         errorIndex.push('Phone Number Must Contain 11 digits Only');
+//         error.innerHTML = errorIndex[2];
+//     }else if(phoneNumber < 11){
+//         errorIndex.push('Phone Number must be 11 digits only');
+//         error.innerHTML = errorIndex[3];
+//     }else if(carPlateNumber > 9 || carPlateNumber < 9){
+//         errorIndex.push('Plate Number Length is incorrect');
+//         error.innerHTML = errorIndex[4];
+//     }else if(!carPlateNumber.match(plateNumber)){
+//         errorIndex.push('Plate Number is incorrect');
+//         error.innerHTML = errorIndex[5];
+//     }
+
+
+
+
+// }
